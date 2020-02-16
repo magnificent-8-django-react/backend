6a6a7fae7a2eb21cc45b6e80388f90163ebef3bc
snowhero <xionghiroyuki@gmail.com>
2020-02-15 17:40:20 -0800

    udate: untrack files in .gitignore

--- backend/main/settings.py
+++ backend/main/settings.py
@@ -95,7 +95,7 @@ DATABASES = {
         'ENGINE' : 'django.db.backends.postgresql',
         'NAME' : 'backend',
         'USER' : 'postgres',
-        'PASSWORD' : '12345678',
+        'PASSWORD' : 'Hiro1234Xiong',
         'HOST' : '127.0.0.1',
         'PORT' : '5432',
     }
